db7ab0a236ab4b194e9136e79c2c0d83d5b9058a
lxf <lxf@lanqiao.org>
2019-08-18 20:04:25 +0800

    对优惠券的增删查改

--- src/main/java/com/mall/food/pojo/Coupon.java
+++ src/main/java/com/mall/food/pojo/Coupon.java
@@ -5,7 +5,9 @@ import lombok.Data;
 public class Coupon {
     private Integer coupon;
     private String describes;
-    private Integer condition;
+    private Integer toplimit;
     private Integer amount;
+    private Integer numbers;
     private String icon;
+
 }
