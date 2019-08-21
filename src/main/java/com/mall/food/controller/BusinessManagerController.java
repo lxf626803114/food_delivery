7e86baf43485cd6c1f297a036dac6f06f5fefcf6
lxf <lxf@lanqiao.org>
2019-08-21 15:00:32 +0800

    模糊分页查询

--- src/main/java/com/mall/food/controller/BusinessManagerController.java
+++ src/main/java/com/mall/food/controller/BusinessManagerController.java
@@ -1,5 +1,7 @@
 package com.mall.food.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.mall.food.mapper.BusinessMapper;
 import com.mall.food.pojo.Business;
 import com.mall.food.service.BusinessmManagerService;
@@ -7,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,34 +34,21 @@ public class BusinessManagerController {
         return "redirect:/business_list";
     }
 @RequestMapping("/businessid")
-    public  String  LikeBusiness(String bId , ModelMap map){
-      List<Business> businesses=   businessMapper.selectLikeBusiness(bId);
+    public  String  LikeBusiness(String bId , ModelMap map,@RequestParam(value = "p",defaultValue = "1") Integer pageNum){
+List<Business> businesses=null;
+      if(!bId.equals("")&&bId!=null){
+          PageHelper.startPage(pageNum,5);
+          businesses=businessMapper.selectAll();
+      }else{
+
+          PageHelper.startPage(pageNum,1);
+          businesses=businessMapper.selectLikeBusiness(bId);
+      }
+     PageInfo<Business> page=new PageInfo<>(businesses);
+
+     map.addAttribute("page",page);
       map.addAttribute("businesses",businesses);
       return "business_list";
     }
-    @GetMapping("businesspage/{num}")
-    public String  userList(Model model,@PathVariable Integer num){
-        if(num==null){
-            num=1;
-        }
-        //所有数据
-        List<Business> businesses =businessMapper.selectAll();
-        //页面展示数据
-        List<Business> list = new ArrayList<>();
-        for(int i =10;i>0;i--){
-            if ((10*num-i) < businesses.size()){
-                list.add(businesses.get(10*num-i));
-            }
-        }
-        //页码
-        int[] count = new int[businesses.size()/10+1];
-        for (int j = 0; j < businesses.size()/10+1; j++){
-            count[j] = j+1;
-        }
 
-        model.addAttribute("count",count);
-        model.addAttribute("size",businesses.size());
-        model.addAttribute("users",list);
-        return  "admin-list";
-    }
 }
